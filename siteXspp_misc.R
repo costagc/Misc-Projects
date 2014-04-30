57f52118bf0fe26d7f0a3b66b907dadb490dc078
Gabriel Costa <gabriel@HERP457-4.local>
2014-04-30 18:06:24 -0400

    test

--- siteXspp_misc.R
+++ siteXspp_misc.R
@@ -1,3 +1,18 @@
 one.dgr <- read.csv('~/Dropbox/Mammal Data/siteXsppXenv_1dgr.csv')
 two.dgr <- read.csv('~/Dropbox/Mammal Data/siteXsppXenv_2dgr.csv')
 
+trait <- read.table('~/Dropbox/Mammal Data/imputedmammals28apr14.txt',h=T)
+head(trait)
+
+sub.one.dgr <- subset(one.dgr, select=colnames(one.dgr) %in% trait$IUCN.binomial)
+sub.fin <- cbind(one.dgr[,1:21],sub.one.dgr)
+head(sub.fin[,1:22])
+
+
+
+sub.2.dgr <- subset(two.dgr, select=colnames(two.dgr) %in% trait$IUCN.binomial)
+sub.fin2 <- cbind(two.dgr[,1:21],sub.2.dgr)
+
+
+
+write.csv(sub.fin2,'~/Dropbox/Mammal Data/siteXsppXenv_sub_2dgr.csv',row.names=F)
